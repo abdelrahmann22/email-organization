038a5a2b821f3ca2664c69e8c7c2b10fb48fdf59
youssef20003 <125611482+youssef20003@users.noreply.github.com>
2024-04-27 22:12:03 +0300

    a

--- project/project/obj/Debug/net8.0-windows/project.AssemblyInfo.cs
+++ project/project/obj/Debug/net8.0-windows/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda491c4165a18a3089c2c73474b3040fbae4dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
