7577b6faeb7e68b8cee1d7886d351b182a3d10a8
youssef20003 <125611482+youssef20003@users.noreply.github.com>
2024-04-28 19:30:21 +0300

    a

--- project/project/Form1.Designer.cs
+++ project/project/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,50 +23,144 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            all = new Button();
+            sent = new Button();
+            reccievd = new Button();
+            spam = new Button();
+            dataGridView1 = new DataGridView();
+            create = new Button();
+            subject = new DataGridViewTextBoxColumn();
+            sender = new DataGridViewTextBoxColumn();
+            time = new DataGridViewTextBoxColumn();
+            view = new DataGridViewButtonColumn();
+            change = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // all
             // 
-            button1.Location = new Point(328, 275);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            all.Location = new Point(52, 22);
+            all.Name = "all";
+            all.Size = new Size(75, 67);
+            all.TabIndex = 1;
+            all.Text = "all";
+            all.UseVisualStyleBackColor = true;
+            all.Click += button1_Click;
             // 
-            // button2
+            // sent
             // 
-            button2.Location = new Point(541, 262);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            sent.Location = new Point(52, 118);
+            sent.Name = "sent";
+            sent.Size = new Size(75, 67);
+            sent.TabIndex = 2;
+            sent.Text = "sent";
+            sent.UseVisualStyleBackColor = true;
+            sent.Click += sent_Click;
+            // 
+            // reccievd
+            // 
+            reccievd.Location = new Point(52, 220);
+            reccievd.Name = "reccievd";
+            reccievd.Size = new Size(75, 67);
+            reccievd.TabIndex = 3;
+            reccievd.Text = "reccievd";
+            reccievd.UseVisualStyleBackColor = true;
+            reccievd.Click += reccievd_Click;
+            // 
+            // spam
+            // 
+            spam.Location = new Point(52, 324);
+            spam.Name = "spam";
+            spam.Size = new Size(75, 67);
+            spam.TabIndex = 4;
+            spam.Text = "spam";
+            spam.UseVisualStyleBackColor = true;
+            spam.Click += spam_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { subject, sender, time, view, change });
+            dataGridView1.Location = new Point(178, 22);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(610, 369);
+            dataGridView1.TabIndex = 5;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            dataGridView1.CellContentDoubleClick += dataGridView1_CellContentClick;
+            dataGridView1.ColumnHeaderMouseClick += dataGridView1_ColumnHeaderMouseClick;
+            // 
+            // create
+            // 
+            create.Location = new Point(178, 398);
+            create.Name = "create";
+            create.Size = new Size(104, 40);
+            create.TabIndex = 6;
+            create.Text = "create folder";
+            create.UseVisualStyleBackColor = true;
+            create.Click += create_Click;
+            // 
+            // subject
+            // 
+            subject.HeaderText = "subject";
+            subject.Name = "subject";
+            // 
+            // sender
+            // 
+            sender.HeaderText = "sender";
+            sender.Name = "sender";
+            // 
+            // time
+            // 
+            time.HeaderText = "time";
+            time.Name = "time";
+            // 
+            // view
+            // 
+            view.HeaderText = "view";
+            view.Name = "view";
+            // 
+            // change
+            // 
+            change.HeaderText = "change";
+            change.Name = "change";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(create);
+            Controls.Add(dataGridView1);
+            Controls.Add(spam);
+            Controls.Add(reccievd);
+            Controls.Add(sent);
+            Controls.Add(all);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
+        private Button all;
+        private Button sent;
+        private Button reccievd;
+        private Button spam;
+        private DataGridView dataGridView1;
+        private Button create;
+        private DataGridViewTextBoxColumn subject;
+        private DataGridViewTextBoxColumn sender;
+        private DataGridViewTextBoxColumn time;
+        private DataGridViewButtonColumn view;
+        private DataGridViewButtonColumn change;
     }
 }
--- project/project/Form1.cs
+++ project/project/Form1.cs
@@ -1,20 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
 namespace project
 {
     public partial class Form1 : Form
     {
+        // Dictionary to map row index to file path
+        private Dictionary<int, string> fileMapping;
+        // Paths to different mail folders
+        String allfile = @"C:\Users\youss\OneDrive\Desktop\bb\profilles\mails\all";
+        string sentfile = @"C:\Users\youss\OneDrive\Desktop\bb\profilles\mails\sent";
+        string recfile = @"C:\Users\youss\OneDrive\Desktop\bb\profilles\mails\reccived";
+        string spamfile = @"C:\Users\youss\OneDrive\Desktop\bb\profilles\mails\spam";
+
         public Form1()
         {
             InitializeComponent();
+            // Initialize the dictionary
+            fileMapping = new Dictionary<int, string>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Code to be executed when the form loads
         }
 
+        // Button click event to create a new mail
+        private void create_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        // Button click event to load all mails
         private void button1_Click(object sender, EventArgs e)
         {
+            dataGridView1.Rows.Clear();
+            RefreshTextBox(allfile);
+        }
 
+        // Button click event to load sent mails
+        private void sent_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            RefreshTextBox(sentfile);
+        }
+
+        // Button click event to load received mails
+        private void reccievd_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            RefreshTextBox(recfile);
+        }
+
+        // Button click event to load spam mails
+        private void spam_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            RefreshTextBox(spamfile);
+        }
+
+        // Method to refresh the DataGridView with file information from a given folder
+        private void RefreshTextBox(string filepath)
+        {
+            // Clear the file mapping dictionary
+            fileMapping.Clear();
+
+            // Get all files in the specified folder
+            string[] files = Directory.GetFiles(filepath);
+
+            // Iterate through each file
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = new FileInfo(files[i]);
+                string firstLine = ReadFirstLine(files[i]); // Read the first line of the file
+                string FileName = Path.GetFileNameWithoutExtension(files[i]); // Get the file name without extension
+                DateTime creationTime = file.CreationTime; // Get the creation time of the file
+                // Add file information to the DataGridView
+                this.dataGridView1.Rows.Add(firstLine, FileName, creationTime);
+                // Add mapping of row index to file path
+                fileMapping.Add(i, files[i]);
+            }
+        }
+
+        // DataGridView cell content click event
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if the "change" button is clicked
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "change")
+            {
+                MessageBox.Show("aa"); // Display a message box
+            }
+            // Check if the "view" button is clicked
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "view")
+            {
+                OpenFile(e.RowIndex); // Open the selected file
+            }
+        }
+
+        // Method to open the file associated with the selected row in DataGridView
+        private void OpenFile(int lineIndex)
+        {
+            // Check if the file mapping dictionary contains the selected row index
+            if (fileMapping.ContainsKey(lineIndex))
+            {
+                string filePath = fileMapping[lineIndex]; // Get the file path from the dictionary
+                System.Diagnostics.Process.Start("explorer.exe", filePath); // Open the file using the default application
+            }
+        }
+
+        // DataGridView column header mouse click event
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            // Check if the "Creation Time" header is clicked
+            if (dataGridView1.Columns[e.ColumnIndex].HeaderText == "Creation Time")
+            {
+                // Sort the DataGridView by creation time in ascending order
+                dataGridView1.Sort(dataGridView1.Columns[e.ColumnIndex], ListSortDirection.Ascending);
+            }
+        }
+
+        // Method to read the first line of a file
+        private string ReadFirstLine(string filePath)
+        {
+            string firstLine = string.Empty;
+
+            // Read the first line of the file
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                firstLine = reader.ReadLine();
+            }
+            return firstLine;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // This method is not currently being used
         }
     }
 }
